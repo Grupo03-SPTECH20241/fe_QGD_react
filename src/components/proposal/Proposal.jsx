3fba4d86a14c6b6dad297a44a14d57ed613f0262
KevynRochaAl <kevyn.alves@sptech.school>
2024-05-27 06:37:10 -0300

    Leves alterações na coloração e tamanho de fonte

--- src/components/proposal/Proposal.jsx
+++ src/components/proposal/Proposal.jsx
@@ -9,7 +9,7 @@ const Proposal = ({ imgProposal, imgMission }) => {
                 <div className={styles["proposalText"]}>
                     <h2>Nossa Proposta</h2>
                     <p>
-                        Com a Quando Geniais se Destacam você vai além! Oferecemos um serviço personalizado para cada cliente, conforme sua necessidade. Entendemos o seu negócio e te auxiliamos a crescer seu negócio exponencialmente!
+                        Com a Quando Geniais se Destacam você vai além! Oferecemos um serviço personalizado para cada cliente, conforme sua necessidade. Entendemos o seu negócio e te auxiliamos a crescer seu negócio exponencialmente! <br />
                         Somos uma startup focada em soluções tecnológicas que desenvolve plataformas personalizadas para impulsionar o sucesso de pequenos negćicios. Te ajudando a aumentar suas vendas e lucros, fortalecer sua marca no mercado e a realizar a gestão do seu negócio.
                     </p>
                 </div>
@@ -22,7 +22,7 @@ const Proposal = ({ imgProposal, imgMission }) => {
                     <h2>Nossa missão</h2>
                     <p>
                         Em parceria com a Organização das Nações Unidas, somos uma empresa compromissada com o Objetivo de Desenvolvimento Sustentável 8 - Trabalho decente e crescimento economômico.
-                        A ODS 8 promove o trabalho adequadamente remunerado, exercido em condições de liberdade, equidade e segurança, capaz de garantir uma vida digna.
+                        A ODS 8 promove o trabalho adequadamente remunerado, exercido em condições de liberdade, equidade e segurança, capaz de garantir uma vida digna. <br />
                         Com isso nós garantimos promover o desenvolvimento da geração de trabalhos dignos, com o crescimento das micro, pequenas e médias empresas com inovações tecnológicas.
                     </p>
                 </div>
